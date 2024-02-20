1ba43da63dd103f87f99db76f2e9c7e143df1542
pushpraj-verma <160465627+pushpraj-verma@users.noreply.github.com>
2024-02-20 14:52:21 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("hi my name is PRV")
+print('this is my first program!')
+print('in this i am going to show you how to make simple calculator!')
+import math
+print("area of reaangle ")
+length = int (input('enter the value of a'))
+
+ print (a*b)    
